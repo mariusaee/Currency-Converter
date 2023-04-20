ab694e6cc37a743cfa33c5be7a83a11f1f890754
Marius <mariusae@me.com>
2023-04-20 16:27:06 +0400

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Marius Malyshev on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
